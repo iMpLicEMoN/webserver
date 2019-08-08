7401751f6b8e2b183feca4794cc6099d131e954a
Mukhtar Musaev <mukhtarmusaev@yandex.ru>
2019-08-08 12:23:52 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+const express = require('express')
+const app = express()
+const port = 3000
+app.get(‘/’, (request, response) => {
+    response.send('Hello from Express!')
+    console.log('request')
+})
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+    console.log(`server is listening on ${port}`)
+})
