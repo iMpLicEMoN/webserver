29d55427f3a7e6e915909a4046d48b0146d04749
Mukhtar Musaev <mukhtarmusaev@yandex.ru>
2019-08-08 12:24:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-app.get(‘/’, (request, response) => {
+app.get('/', (request, response) => {
     response.send('Hello from Express!')
     console.log('request')
 })
